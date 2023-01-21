3ae4ee8cf5e8ebe64b2d6e3743e33fd620cf6c1b
sudarshan441 <sudarshanpujari812@gmail.com>
2023-01-21 12:43:57 +0530

    added

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -5,7 +5,8 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-});
+},{
+timestamps:true,});
 
 const UserModel = mongoose.model("user", UserSchema);
 
